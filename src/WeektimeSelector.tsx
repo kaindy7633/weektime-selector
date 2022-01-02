c48cf1cba37590fe85307d82668c60a5a3285df0
kaindy7633 <kaindy7633@gmail.com>
2022-01-02 08:59:09 +0800

    调整目录结构，删除无用的练习代码，新增测试文件

--- src/WeektimeSelector.tsx
+++ src/WeektimeSelector.tsx
@@ -1,7 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { IWeektimeSelectorProps } from './interface';
+import { IWeektimeSelectorProps, ITdData } from './interface';
 
 const WeektimeSelector: React.FC<IWeektimeSelectorProps> = () => {
+  const [theadRange, setTheadRange] = useState<Array<number>>([]); // 时间Range
+  const [mouseMode, setMouseMode] = useState<number>(0);
+  const [width, setWidth] = useState<number>(0);
+  const [height, setHeight] = useState<number>(0);
+  const [left, setLeft] = useState<number>(0);
+  const [top, setTop] = useState<number>(0);
+  const [cRow, setCrow] = useState<number>(0); // 当前行
+  const [cCol, setCcol] = useState<number>(0); // 当前列
+  const [selected, setSelected] = useState<boolean>(false);
+
+  useEffect(() => {
+    // 生成时间 Range
+    setTheadRange(createRange(24));
+  }, []);
+
   return <></>;
 };
 
---
+++ test/weektime-selector.test.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
+
+describe('should be test weektime selector', () => {});
