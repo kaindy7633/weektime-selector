7833de9db3a9b5c2e8f0cd12d4d41e81eb6f36df
kaindy7633 <kaindy7633@gmail.com>
2022-01-09 11:25:57 +0800

    调整目录结构和部分函数方法，本地调试OK，可访问：localhost:1234

--- example/App.tsx
+++ example/App.tsx
@@ -1,100 +1,93 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import WeektimeSelector from '../.';
-import { createChild, filterChild, weekList } from './utils';
-import { cloneDeep } from 'lodash';
+// import { useEffect, useState } from 'react';
+import WeektimeSelector from '../src/WeektimeSelector';
+// import { createChild, filterChild, weekList } from './utils';
+// import { cloneDeep } from 'lodash';
 import React = require('react');
 
-export interface IWeektimeDataItem {
-  value: string;
-  row: number;
-  child: ITdData[];
-}
+// export interface IWeektimeDataItem {
+//   value: string;
+//   row: number;
+//   child: ITdData[];
+// }
 
-export interface ISelectedData {
-  id: number;
-  name: string;
-  value: string | void;
-}
+// export interface ISelectedData {
+//   id: number;
+//   name: string;
+//   value: string | void;
+// }
 
 const App: React.FC = () => {
-  const [weektimeData, setWeektimeData] = useState<IWeektimeDataItem[]>(
-    weekList.map((_value, _index) => {
-      return {
-        value: _value,
-        row: _index,
-        child: createChild(_value, _index, 48),
-      };
-    })
-  );
+  // const [weektimeData, setWeektimeData] = useState<IWeektimeDataItem[]>(
+  //   weekList.map((_value, _index) => {
+  //     return {
+  //       value: _value,
+  //       row: _index,
+  //       child: createChild(_value, _index, 48),
+  //     };
+  //   })
+  // );
 
-  const [selectedData, setSelectedData] = useState<ISelectedData[]>(
-    weektimeData.map((_item: IWeektimeDataItem) => {
-      return {
-        id: _item.row,
-        name: _item.value,
-        value: filterChild(_item.child),
-      };
-    })
-  );
+  // const [selectedData, setSelectedData] = useState<ISelectedData[]>(
+  //   weektimeData.map((_item: IWeektimeDataItem) => {
+  //     return {
+  //       id: _item.row,
+  //       name: _item.value,
+  //       value: filterChild(_item.child),
+  //     };
+  //   })
+  // );
 
-  useEffect(() => {
-    // 当 weektimeData 发生变化时，更新 selectedData
-    setSelectedData(
-      weektimeData.map((_item: IWeektimeDataItem) => {
-        return {
-          id: _item.row,
-          name: _item.value,
-          value: filterChild(_item.child),
-        };
-      })
-    );
-  }, [weektimeData]);
+  // useEffect(() => {
+  //   // 当 weektimeData 发生变化时，更新 selectedData
+  //   setSelectedData(
+  //     weektimeData.map((_item: IWeektimeDataItem) => {
+  //       return {
+  //         id: _item.row,
+  //         name: _item.value,
+  //         value: filterChild(_item.child),
+  //       };
+  //     })
+  //   );
+  // }, [weektimeData]);
 
-  // 清除已选数据
-  const clearData = () => {
-    weektimeData.forEach((_item) => {
-      _item.child.forEach((_c: any) => {
-        _c.selected = false;
-      });
-    });
-  };
+  // // 清除已选数据
+  // const clearData = () => {
+  //   weektimeData.forEach((_item) => {
+  //     _item.child.forEach((_c: any) => {
+  //       _c.selected = false;
+  //     });
+  //   });
+  // };
 
   // 选中黄金时间
-  const selectGoldTime = (type: string) => {
-    const _cloneData = cloneDeep(weektimeData);
-    const wLen = _cloneData.length;
+  // const selectGoldTime = (type: string) => {
+  //   const _cloneData = cloneDeep(weektimeData);
+  //   const wLen = _cloneData.length;
 
-    const minTime = 18;
-    const maxTime = 41;
-    for (let i = 0; i < wLen; i++) {
-      if (type === 'work-day' && i < 5) {
-        for (let j = 0; j < _cloneData[i].child.length; j++) {
-          if (j >= minTime && j <= maxTime) {
-            _cloneData[i].child[j].selected = true;
-          }
-        }
-      } else if (type === 'home-day' && i > 4) {
-        for (let j = 0; j < _cloneData[i].child.length; j++) {
-          if (j >= minTime && j <= maxTime) {
-            _cloneData[i].child[j].selected = true;
-          }
-        }
-      }
-    }
-    setWeektimeData(_cloneData);
-  };
+  //   const minTime = 18;
+  //   const maxTime = 41;
+  //   for (let i = 0; i < wLen; i++) {
+  //     if (type === 'work-day' && i < 5) {
+  //       for (let j = 0; j < _cloneData[i].child.length; j++) {
+  //         if (j >= minTime && j <= maxTime) {
+  //           _cloneData[i].child[j].selected = true;
+  //         }
+  //       }
+  //     } else if (type === 'home-day' && i > 4) {
+  //       for (let j = 0; j < _cloneData[i].child.length; j++) {
+  //         if (j >= minTime && j <= maxTime) {
+  //           _cloneData[i].child[j].selected = true;
+  //         }
+  //       }
+  //     }
+  //   }
+  //   setWeektimeData(_cloneData);
+  // };
 
   return (
     <div className="App">
       <h2 className="app-title">日期时间选择组件(React版本)</h2>
-      <WeektimeSelector
-        weektimeData={weektimeData}
-        selectedData={selectedData}
-        setSelectedData={setSelectedData}
-        clearData={clearData}
-        selectGoldTime={selectGoldTime}
-      />
+      <WeektimeSelector />
     </div>
   );
 };
